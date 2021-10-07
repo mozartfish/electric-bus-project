d3020b6f543813df9d06fa82cb2f8086275a6d60
unknown <jiuying.han@utah.edu>
2021-10-06 19:26:19 -0600

    histogram and line chart

--- drawPlots.js
+++ drawPlots.js
@@ -9,8 +9,8 @@ class plotting{
         // slice the first 5 records for now (before adding the interaction)
         const dataset= this.electricStatData.slice(0,5)
         //console.log("slice dataset",dataset)
-        const dataset1= this.p60Data
-        console.log("p60", dataset1)
+        const dataset1= this.p60Data['Bus Distance']['1000']
+        //console.log("p60", [...Array(dataset1.length).keys()])
 
         const width = 300;
         
@@ -39,91 +39,90 @@ class plotting{
       dimensions.height - dimensions.margin.top - dimensions.margin.bottom
   
     
-      const metricAccessor = d => d['Ei']
-      const yAccessor = d => d["block_num"]
+
+// draw the enviroment equity histogram (eq)
+      const eqXAccessor = d => d['Ei']
+      const eqYAccessor = d => d["block_num"]
+      //console.log("accessor",eqXAccessor(dataset[0]))
 
 
-        
-
-
-    // draw histogram (environmental equity)
-        const svg = d3
+        const eqSvg = d3
             .select('#data-charts')
             .append('svg')
             .attr('width', dimensions.width)
-            .attr('height', dimensions.height);
+            .attr('height', dimensions.height)
+            .attr('id', 'eqHistogram');
 
 
-        const graph = svg.append('g')
+        const eqGraph = eqSvg.append('g')
             .attr('width', dimensions.width)
             .attr('height', dimensions.height)
             .attr('transform', `translate(${dimensions.margin.left}, ${dimensions.margin.top})`);
             
-        const gXAxis = graph.append('g')
+        const eqgXAxis = eqGraph.append('g')
             .style('transform', `translateY(${dimensions.boundedHeight}px)`)
         
-        const gYAxis = graph.append('g')
+        const eqgYAxis = eqGraph.append('g')
 
 
-        const yScale = d3
+        const eqYScale = d3
             .scaleLinear()
             //.domain([0, d3.max(dataset, d => d['Ei'])]) 
-            .domain([0, d3.max(dataset, metricAccessor)])
+            .domain([0, d3.max(dataset, eqXAccessor)])
             .range([dimensions.boundedHeight, 0])
             // .nice()
 
-        const xScale = d3.scaleBand()
-            .domain(dataset.map(yAccessor))
+        const eqXScale = d3.scaleBand()
+            .domain(dataset.map(eqYAccessor))
             .range([0, dimensions.boundedWidth])
             .paddingInner(0.2)
             .paddingOuter(0.2);
 
 
            
-        const rects = graph.selectAll('rect')
+        const eqRects = eqGraph.selectAll('rect')
             .data(dataset)
             .enter()
             .append("rect")
-            .attr('width', xScale.bandwidth)
+            .attr('width', eqXScale.bandwidth)
             .attr('class', 'bar-rect')
-            .attr('height', d => dimensions.boundedHeight - yScale(metricAccessor(d)))
-            .attr('x', d => xScale(yAccessor(d)))
-            .attr('y', d => yScale(metricAccessor(d)))
+            .attr('height', d => dimensions.boundedHeight - eqYScale(eqXAccessor(d)))
+            .attr('x', d => eqXScale(eqYAccessor(d)))
+            .attr('y', d => eqYScale(eqXAccessor(d)))
             .attr('fill', 'cornflowerblue')
 
-        const xAxis = d3.axisBottom(xScale);
-        const yAxis = d3.axisLeft(yScale)
+        const eqXAxis = d3.axisBottom(eqXScale);
+        const eqYAxis = d3.axisLeft(eqYScale)
                         .ticks(5)
-        gXAxis.call(xAxis);
-        gYAxis.call(yAxis);
+        eqgXAxis.call(eqXAxis);
+        eqgYAxis.call(eqYAxis);
 
-        gXAxis
+        eqgXAxis
         .selectAll('text')
         .style('font-size', 10)
         // .append("text")
         // .text(yAccessor)
         
-        gYAxis.selectAll('text')
+        eqgYAxis.selectAll('text')
             .style('font-size', 10);
         
 
-        const xAxisLabel = gXAxis
+        const eqXAxisLabel = eqgXAxis
             .append('text')
             .attr('x', dimensions.boundedWidth / 2)
             .attr('y', dimensions.margin.bottom - 10)
             .attr('fill', 'black')
             .style('font-size', '1.4em')
-            // Use the supplied metric for our text; however in most cases you'd perform a lookup in a proper map
-            .text("Bus number")
+            .text("Bus Number")
             .style("text-transform", "capitalize")
 
-        const yAxisLabel = gYAxis.append("text")
+        const eqYAxisLabel = eqgYAxis.append("text")
             // Draw this in the middle of the y axis and just inside the left side of the chart wrapper
             .attr("x", -dimensions.boundedHeight / 2)
             .attr("y", -dimensions.margin.left + 10)
             .attr("fill", "black")
             .style("font-size", "1.4em")
-            .text("Environmental equity")
+            .text("Environmental Equity")
             // Rotate the label to find next to the y axis
             .style("transform", "rotate(-90deg)")
             // Rotate the label around its center
@@ -137,81 +136,88 @@ class plotting{
 
 
 
-//     // draw line chart
-//     const wrapper = d3.select("#wrapper")
-//     .append("svg")
-//       .attr("width", dimensions.width)
-//       .attr("height", dimensions.height)
+    // draw line chart for the mileage
 
-//   // We can draw our chart inside of a "g" element and shift it all at once using the CSS transform property
-//   const bounds = wrapper.append("g")  // Think of "g" as the "div" equivalent within an SVG element
-//     .style("transform", `translate(${
-//       dimensions.margin.left
-//     }px, ${
-//       dimensions.margin.top
-//     }px)`)
+    const mileXAccessor = d => dataset1.indexOf(d)//[...Array(d.length).keys()]
+    const mileYAccessor = d => d
+    //console.log("mile",mileYAccessor(dataset1[1]))
 
-//   // Create our scales
-//   const yScale = d3.scaleLinear()
-//     .domain(d3.extent(dataset, yAccessor))  // d3.extent returns an array with the min and max value
-//     .range([dimensions.boundedHeight, 0])   // range refers to the highest and lowest numbers to display
+    const mileSvg = d3
+        .select('#data-charts')
+        .append('svg')
+        .attr('width', dimensions.width)
+        .attr('height', dimensions.height)
+        .attr('id', 'mileLineChart');
 
-//   // Let's visualize the threshold by adding a rectangle to cover all temperatures below freezing
-//   const freezingTemperaturePlacement = yScale(32) // 32 degrees Fahrenheit
-//   const freezingTemperatures = bounds.append("rect")
-//     .attr("x", 0)
-//     .attr("width", dimensions.boundedWidth)
-//     .attr("y", freezingTemperaturePlacement)
-//     .attr("height", dimensions.boundedHeight - freezingTemperaturePlacement)
-//     .attr("fill", "#e0f3f3")  // Make the rectangle frosty to indicate freezing
 
-//   const xScale = d3.scaleTime()
-//     .domain(d3.extent(dataset, xAccessor))  // Use a time scale since we are working with date objects
-//     .range([0, dimensions.boundedWidth])
+  const mileBounds = mileSvg.append("g") 
+    .style("transform", `translate(${
+      dimensions.margin.left
+    }px, ${
+      dimensions.margin.top
+    }px)`)
 
-//   /*
-//     “The d attribute will take a few commands that can be capitalized (if giving an absolute value) or lowercased (if giving a relative value):
+  // Create scales
+  const mileYScale = d3.scaleLinear()
+    .domain(d3.extent(dataset1, mileYAccessor))  // d3.extent returns an array with the min and max value
+    .range([dimensions.boundedHeight, 0])   // range refers to the highest and lowest numbers to display
 
-//     M will move to a point (followed by x and y values)
-//     L will draw a line to a point (followed by x and y values)
-//     Z will draw a line back to the first point”
+ 
+  const mileXScale = d3.scaleLinear()
+    .domain([0, dataset1.length])  
+    .range([0, dimensions.boundedWidth])
 
-//     Excerpt From: Nate Murray. “Fullstack Data Visualization with D3.” Apple Books.
+ 
+  const mileLineGenerator = d3.line() // Creates a generator that converts data points into a d string
+    // Transform our data point with the appropriate accessor and the scale to get the scaled value in pixel space
+    .x(d => mileXScale(mileXAccessor(d)))
+    .y(d => mileYScale(mileYAccessor(d)))
 
-//     bounds.append("path").attr("d", "M 0 0 L 100 0 L 100 100 L 0 50 Z")
+  const mileLine = mileBounds.append("path")
+    // Feed our dataset to our line generator function
+    .attr("d", mileLineGenerator(dataset1))
+    // SVG elements default to a black fill and no stroke; which gives us a filled in shape unless we add styling
+    .attr("fill", "none")
+    .attr("stroke", "#af9358")
+    .attr("stroke-width", 2)
 
-//   */
-//   const lineGenerator = d3.line() // Creates a generator that converts data points into a d string
-//     // Transform our data point with the appropriate accessor and the scale to get the scaled value in pixel space
-//     .x(d => xScale(xAccessor(d)))
-//     .y(d => yScale(yAccessor(d)))
+  // Draw y axis tick marks and labels
+  const mileYAxisGenerator = d3.axisLeft()  // We want labels of the y-axis to be to the left of the axis line
+    .scale(mileYScale)
+  // Our axis generator will create lots of element; create a g element to contain them and keep our DOM organized
+  const mileYAxis = mileBounds.call(mileYAxisGenerator)
 
-//   const line = bounds.append("path")
-//     // Feed our dataset to our line generator function
-//     .attr("d", lineGenerator(dataset))
-//     // SVG elements default to a black fill and no stroke; which gives us a filled in shape unless we add styling
-//     .attr("fill", "none")
-//     .attr("stroke", "#af9358")
-//     .attr("stroke-width", 2)
+  // Draw x axis tick marks and labels
+  const mileXAxisGenerator = d3.axisBottom()  // We want our labels of the x-axis to appear under the axis line
+    .scale(mileXScale)
+  const mileXAxis = mileBounds.append("g")
+    .call(mileXAxisGenerator)
+         .style("transform", `translateY(${
+        dimensions.boundedHeight
+      }px)`)
 
-//   // Additional things to draw (tick marks, labels, legends, etc)
 
-//   // Draw y axis tick marks and labels
-//   const yAxisGenerator = d3.axisLeft()  // We want labels of the y-axis to be to the left of the axis line
-//     .scale(yScale)
-//   // Our axis generator will create lots of element; create a g element to contain them and keep our DOM organized
-//   const yAxis = bounds.call(yAxisGenerator)
 
-//   // Draw x axis tick marks and labels
-//   const xAxisGenerator = d3.axisBottom()  // We want our labels of the x-axis to appear under the axis line
-//     .scale(xScale)
-//   const xAxis = bounds.append("g")
-//     .call(xAxisGenerator)
-//     // If you stop here, the xAxisGenerator knows how to display tick marks and labels relative to the axis line, but we need to move it to the bottom with a CSS transform
-//       .style("transform", `translateY(${
-//         dimensions.boundedHeight
-//       }px)`)
+  const mileXAxisLabel = mileXAxis
+      .append('text')
+      .attr('x', dimensions.boundedWidth / 2)
+      .attr('y', dimensions.margin.bottom - 10)
+      .attr('fill', 'black')
+      .style('font-size', '1.4em')
+      .text("Stop Number")
+      .style("text-transform", "capitalize")
 
+  const mileYAxisLabel = mileYAxis.append("text")
+      // Draw this in the middle of the y axis and just inside the left side of the chart wrapper
+      .attr("x", -dimensions.boundedHeight / 2)
+      .attr("y", -dimensions.margin.left + 10)
+      .attr("fill", "black")
+      .style("font-size", "1.4em")
+      .text("Mileage Before Recharge")
+      // Rotate the label to find next to the y axis
+      .style("transform", "rotate(-90deg)")
+      // Rotate the label around its center
+      .style("text-anchor", "middle")
   
       }
 }
