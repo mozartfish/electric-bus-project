56b2234bf2cd04b97b8a806c8ef5cb1157525881
Pranav Rajan <rajan.pranav320@gmail.com>
2021-10-21 18:39:52 -0600

    add in a chart

--- busTable.js
+++ busTable.js
@@ -34,7 +34,54 @@ class busTable {
     });
     console.log(busObjects);
 
-    
+    // Set up accessor functions
+    const formatDistance = (d) => +d;
+    const formatBusName = (d) => +d;
+
+    // Set up table information
+    const numberOfRows = busObjects.length;
+    const columns = [
+      {
+        label: 'Bus ID',
+        type: 'number',
+        format: (d) => formatBusName(d.busName),
+      },
+      {
+        label: 'Max Distance Traveled',
+        type: 'number',
+        format: (d) => formatDistance(d.distanceMax),
+      },
+    ];
+
+    // build table
+    const table = d3.select('#table');
+    table
+      .append('thead')
+      .append('tr')
+      .selectAll('thead')
+      .data(columns)
+      .join('th')
+      .text((d) => d.label)
+      .attr('class', (d) => d.type);
+
+    const body = table.append('tbody');
+    busObjects.forEach((d) => {
+      body
+        .append('tr')
+        .selectAll('td')
+        .data(columns)
+        .join('td')
+        .text((column) => column.format(d))
+        .attr('class', (column) => column.type)
+        .style(
+          'background',
+          (column) => column.background && column.background(d)
+        )
+        .style(
+          'transform',
+          (column) => column.transform && column.transform(d)
+        );
+    });
   }
 
   updateData(newData) {
