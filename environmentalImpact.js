e32c1c2764d3ea7501713103db5eed03dabfb124
Pranav Rajan <rajan.pranav320@gmail.com>
2021-10-25 13:01:41 -0600

    finish bar chart

--- environmentalImpact.js
+++ environmentalImpact.js
@@ -4,15 +4,82 @@ class environmentalImpact {
   }
 
   drawHistogram() {
-    // console.log('histogram data');
-    // console.log(this.data);
+    console.log('histogram data');
+    console.log(this.data);
 
-    // // accessor functions
-    // const xAccessor = (d) => +d.block_num;
-    // const yAccessor = (d) => +d.Ei;
+    // accessor functions
+    const xAccessor = (d) => +d.block_num;
+    const yAccessor = (d) => +d.Ei;
+
+    // margins
+    const margins = {
+      top: 10,
+      right: 10,
+      bottom: 100,
+      left: 40,
+    };
+
+    const width = 6000 - margins.left - margins.right;
+    const height = 700 - margins.top - margins.bottom;
+
+    // remove all the rectangles and scale
+
+    const svg = d3
+      .select('#ei-histogram')
+      .select('#ei-svg')
+      .attr('width', width + margins.left + margins.right)
+      .attr('height', height + margins.top + margins.bottom);
+
+    svg.select('#bars').remove();
+    svg.select('#x-axis').remove();
+    svg.select('#y-axis').remove();
+
+    const xScale = d3
+      .scaleBand()
+      .domain(this.data.map((d) => d.block_num))
+      .range([0, width])
+      .padding(0.2);
+
+    svg
+      .append('g')
+      .attr('id', 'x-axis')
+      .attr('transform', `translate(45, ${height + 15})`)
+      .call(d3.axisBottom(xScale))
+      .selectAll('text')
+      .style('text-anchor', 'end')
+      .style('font-size', '18px')
+      .attr('transform', 'rotate(-45) translate(-10, -5)');
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(this.data, yAccessor)])
+      .range([height, 0])
+      .nice();
+    svg
+      .append('g')
+      .attr('transform', `translate(45, 15)`)
+      .attr('id', 'y-axis')
+      .call(d3.axisLeft(yScale));
+
+    svg
+      .append('g')
+      .attr('id', 'bars')
+      .attr('transform', `translate(45, 0)`)
+      .selectAll('rect')
+      .data(this.data)
+      .join('rect')
+      .attr('width', xScale.bandwidth())
+      .attr('class', 'bar-rect')
+      .attr('height', (d) => height - yScale(d.Ei))
+      .attr('x', (d) => xScale(d.block_num))
+      .attr('y', (d) => yScale(d.Ei))
+      .attr('fill', 'cornflowerblue');
+
+    //   .select('#ei-histogram')
+    //   .select('#ei-svg')
 
     // // chart dimensions
-    // const width = 200;
+    // const width = 800;
     // let dimensions = {
     //   width: width,
     //   height: width * 1.5,
@@ -33,7 +100,7 @@ class environmentalImpact {
     // // draw canvas
     // const wrapper = d3
     //   .select('#ei-histogram')
-    //   .append('svg')
+    //   .select('#ei-svg')
     //   .attr('width', dimensions.width)
     //   .attr('height', dimensions.height);
 
